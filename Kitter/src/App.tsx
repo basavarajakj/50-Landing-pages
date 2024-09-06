26ff1bbf730af2ad3d5fb0e70fee6d87eb5b8f6c
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-09-06 13:59:15 +0530

    added offer section

--- Kitter/src/App.tsx
+++ Kitter/src/App.tsx
@@ -1,6 +1,7 @@
 import Category from "./components/Category"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Offers from "./components/Offers"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Hero />
       <Category />
+      <Offers />
     </>
   )
 }
---
+++ Kitter/src/components/Offers.tsx
@@ -0,0 +1,67 @@
+import type { CSSProperties } from 'react';
+
+import offerBanner1 from '../assets/offer-banner-1.jpg';
+import offerBanner2 from '../assets/offer-banner-2.jpg';
+import offerBanner3 from '../assets/offer-banner-3.jpg';
+
+const OFFER_BANNER = [
+  {
+    id: 1,
+    imageUrl: offerBanner1,
+    cardTitles: {
+      title1: "Hot Summer",
+      title2: "Deals"
+    },
+    cardSubtitle: "Selected Items. Online Only." 
+  },
+  {
+    id: 2,
+    imageUrl: offerBanner2,
+    cardTitles: {
+      title1: "Spoil your true",
+      title2: "love"
+    },
+    cardSubtitle: "Treats & Grooming." 
+  },
+  {
+    id: 3,
+    imageUrl: offerBanner3,
+    cardTitles: {
+      title1: "New in this",
+      title2: "Year"
+    },
+    cardSubtitle: "Our Brand You Will Love." 
+  },
+]
+
+const Offers = () => {
+  return (
+    <section className="offers section" aria-label="offer">
+      <div className="container">
+
+        <div className="grid-list">
+
+          {OFFER_BANNER.map(offer => (
+            <li>
+                <div className="offer-card has-bg-image img-holder" style={{backgroundImage : `url(${offer.imageUrl})`, "--width" : 540, "--height": 374} as CSSProperties}>
+
+                  <p className="card-subtitle">{offer.cardSubtitle}</p>
+
+                  <h3 className="h3 card-title">
+                    {offer.cardTitles.title1} <span className="span">{offer.cardTitles.title2}</span>
+                  </h3>
+
+                  <button className='btn'>Read More</button>
+
+                </div>
+            </li>
+          ))}
+
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Offers
